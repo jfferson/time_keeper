f2fca28ee45a23f910e5adf48765b07c0ade2f55
jeferson <jeff.lemos.a@gmail.com>
2022-05-21 16:19:57 -0300

    tentando resolver unordered_map

---
+++ src/time-keeper.h
@@ -0,0 +1,48 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * time-keeper.h
+ * Copyright (C) 2022 Unknown <jeff.lemos.a@gmail.com>
+ *
+ * time_keeper is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * time_keeper is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TIME_KEEPER_H_
+#define _TIME_KEEPER_H_
+
+#include <glibmm/datetime.h>
+#include <glibmm/timer.h>
+
+class Time_Keeper
+{
+public:
+	void start_timer(){ timer.start(); };
+	void stop_timer(){ timer.stop(); };
+	void reset_timer(){ timer.reset(); };
+	Glib::ustring display_timer() { return ((Glib::ustring) (std::to_string(((int) (timer.elapsed()/3600)))+\
+	                                                         ":"+std::to_string(((int) (timer.elapsed()/60)))+\
+	                                                         ":"+std::to_string(timer.elapsed() ))); };
+	Time_Keeper(){};
+	//Time_Keeper(){ start_time = Glib::DateTime::create_now_local(); };
+	bool operator == ( Time_Keeper *t) { return t == this; };
+
+protected:
+
+private:
+	Glib::DateTime start_time;
+	Glib::TimeSpan elapsed_time; 
+	Glib::Timer timer;
+};
+
+#endif // _TIME_KEEPER_H_
+
---
+++ src/u-i-controller.h
@@ -0,0 +1,53 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * u-i-controller.h
+ * Copyright (C) 2022 Unknown <jeff.lemos.a@gmail.com>
+ *
+ * time_keeper is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * time_keeper is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _U_I_CONTROLLER_H_
+#define _U_I_CONTROLLER_H_
+
+#include <gtkmm.h>
+#include <unordered_map>
+#include <boost/functional/hash.hpp>
+#include <time-keeper.h>
+
+class UI_Controller
+{
+public:
+	UI_Controller(Gtk::Builder* refference,Gtk::Application * app);
+	void deffine_application(Gtk::Application * app);
+	void add_window_to_application(Gtk::Window * window);
+protected:
+
+private:
+	Gtk::Builder * refference;
+	Gtk::Grid * content_relations;
+	Gtk::Application * app;
+	std::vector<Glib::RefPtr<Glib::Object>> widgets;
+	struct HashTime{ size_t operator () ( std::pair<int, Time_Keeper>& p) const; };
+	std::unordered_map<int,Time_Keeper,HashTime> bind_time;
+	void show_window(Gtk::Window *window);
+	void start_timer(Gtk::Widget * selected, int position);
+	void stop_timer(Gtk::Widget * selected) { return ;};
+	void restart_timer(Gtk::Widget * selected) { return ;};
+	void add_timer(Gtk::Widget * selected) { return ;};
+	
+
+};
+
+#endif // _U_I_CONTROLLER_H_
+
